d0e75197fb054c72d144d8ae10253ee50684fe0d
yagijin <mukailab@mukailabnoMacBook-Pro.local>
2020-02-10 19:44:04 +0900

    Updates

---
+++ precache-manifest.2e725b3ca85ea285565fdc86525d26ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f47b6150f1642c281893083bbdc510c9",
+    "url": "/NG_1904/index.html"
+  },
+  {
+    "revision": "83016f957b73aaeaf131",
+    "url": "/NG_1904/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "a7319abecd8319ef7bab",
+    "url": "/NG_1904/static/css/main.441c99f6.chunk.css"
+  },
+  {
+    "revision": "83016f957b73aaeaf131",
+    "url": "/NG_1904/static/js/2.abd91616.chunk.js"
+  },
+  {
+    "revision": "47f0823269ddc1f531d85f48617139aa",
+    "url": "/NG_1904/static/js/2.abd91616.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7319abecd8319ef7bab",
+    "url": "/NG_1904/static/js/main.30258037.chunk.js"
+  },
+  {
+    "revision": "18ce74fe930c98afeae3",
+    "url": "/NG_1904/static/js/runtime-main.8b391744.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.231180d46b0a561b4debd48b0a140904.js"
+  "/NG_1904/precache-manifest.2e725b3ca85ea285565fdc86525d26ff.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/NG_1904/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
